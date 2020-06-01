31eb16305ebe81d0d9ba6aa010485918e0112a55
jordangarcia <jordan.garcia@gmail.com>
2020-06-01 14:13:55 -0600

    update aliases

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,5 @@
-export PATH="/usr/local/bin:$ZSH/bin:/Users/jordan/.nvm/versions/node/v10.13.0/bin:$PATH"
+export PATH="/usr/local/Cellar/bin:/usr/local/bin:$ZSH/bin:/Users/jordan/.nvm/versions/node/v10.13.0/bin:$PATH"
+export PATH="/Users/jordan/code/flutter/bin:$PATH"
+export PATH="/Users/jordan/.composer/vendor/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
