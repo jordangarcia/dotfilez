63b95e13c0ef9e8058739ab4df38b9f6d3d73b6e
Jordan Garcia <jordan.garcia@gmail.com>
2023-12-28 17:55:11 -0500

    update tabby

--- nvim/lua/plugins/tabby.lua
+++ nvim/lua/plugins/tabby.lua
@@ -1,34 +1,42 @@
 -- local action_state = require "telescope.actions.state"
 -- local z_utils = require "telescope._extensions.zoxide.utils"
 
+local api = vim.api
+
+local function getNvimTreeWidth()
+  for _, win in pairs(api.nvim_tabpage_list_wins(0)) do
+    if vim.bo[api.nvim_win_get_buf(win)].ft == "NvimTree" then
+      return api.nvim_win_get_width(win)
+    end
+  end
+  return 0
+end
+
 ---@type LazyPluginSpec[]
 return {
   {
     "nanozuki/tabby.nvim",
+    enabled = true,
     event = "VeryLazy",
     keys = {
       {
         "<C-t>",
         "<CMD> $tabnew <CR>",
-        desc = "new tab",
+        desc = "New tab",
+      },
+      {
+        "<leader>tq",
+        "<CMD> tabc <CR>",
+        desc = "Tab [q]uit",
       },
     },
     config = function(self, opts)
       local theme = {
         fill = "TabLineFill",
-        -- fill = { fg = "#f2e9de", bg = "#907aa9", style = "italic" },
-        head = "TabLine",
-        current_tab = "TabLineSel",
-        tab = "TabLineOff",
-        win = "TabLine",
-        tail = "TabLine",
       }
+
       require("tabby.tabline").set(function(line)
         return {
-          -- {
-          --   { "  ", hl = theme.head },
-          --   line.sep("", theme.head, theme.fill),
-          -- },
           line.tabs().foreach(function(tab)
             local hl = tab.is_current() and "TabLineSel" or "TabLineOff"
             local iconHl = hl .. "Icon"
@@ -47,41 +55,25 @@ return {
                 win.buf_name(),
                 hl = hl,
               }
-              -- return icon .. " " .. win.buf_name()
             end)
             for i = #wins, 2, -1 do
               table.insert(wins, i, { " | ", hl = sepHl })
             end
 
             return {
-              line.sep("█", hl, theme.fill),
+              line.sep("█", hl, "TabLineFill"),
               -- tab.is_current() and "" or "󰆣",
               tab.number(),
+              " ",
               wins,
-              -- table.concat(wins, " | "),
-              -- tab.name(),
-              -- line.sep("█", hl, theme.fill),
-              line.sep("▊", hl, theme.fill),
+              " ",
+              line.sep("▊", hl, "TabLineFill"),
               hl = hl,
-              margin = " ",
+              margin = "",
             }
           end),
           line.spacer(),
-          -- line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-          --   return {
-          --     line.sep("", theme.win, theme.fill),
-          --     win.is_current() and "" or "",
-          --     win.buf_name(),
-          --     line.sep("", theme.win, theme.fill),
-          --     hl = theme.win,
-          --     margin = " ",
-          --   }
-          -- end),
-          -- {
-          --   line.sep("", theme.tail, theme.fill),
-          --   { "  ", hl = theme.tail },
-          -- },
-          hl = theme.fill,
+          hl = "TabLineFill",
         }
       end)
     end,
