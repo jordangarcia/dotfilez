9bbe2e696d91334415d5f4d337dd97895840b8cb
Jordan Garcia <jordan.garcia@gmail.com>
2024-11-12 14:54:26 -1000

    cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,11 +4,9 @@ require "core"
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
+  require("core.bootstrap").install(lazypath)
 end
 
--- dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 
 -- load plugins
