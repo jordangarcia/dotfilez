3579b81478a470a910da9698d92f6ec08eb97902
Jordan Garcia <jordan.garcia@gmail.com>
2026-01-26 11:00:33 -0800

    kitty, try tmux again, fnm

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -1,26 +1,10 @@
-SHELL_NODE_VERSION="v18.18.2"
-# handles all node / nvm setup
-lazynvm() {
+# fnm (fast node manager) - replaces nvm
+eval "$(fnm env --use-on-cd)"
+
+# keep nvm command available for edge cases (lazy-loaded)
+nvm() {
   unset -f nvm
   export NVM_DIR=~/.nvm
   . "$(brew --prefix nvm)/nvm.sh"
-  #[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-}
-
-nvm() {
-  lazynvm
   nvm $@
 }
-
-
-autoload -U add-zsh-hook
-load-nvmrc() {
-  if [[ -f .nvmrc && -r .nvmrc ]]; then
-    nvm use
-  fi
-}
-
-add-zsh-hook chpwd load-nvmrc
-
-export PATH="/Users/jordan/.nvm/versions/node/$SHELL_NODE_VERSION/bin:$PATH"
-
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -14,5 +14,7 @@ export PATH="$PATH:/Users/jordan/code/google-cloud-sdk/bin"
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export PATH="$PATH:$HOME/.cargo/bin"
+# jorgist
+export PATH="$PATH:$HOME/code/jorgist/bin"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
